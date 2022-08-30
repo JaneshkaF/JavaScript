056a8ce4e6ba7b7cb72d2d1dd6c25414859b322e
JaneshkaF <janeshkaf@aol.com>
2022-08-30 00:33:13 +0000

    final JavaScript

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var subjects = ["My mom", "Bad Bunny", "My brother", "Marcelo", "Aliens"];
+  var action = [" saw", " threw", " ate", " slapped", " broke"];
+  var victim = [
+    " a ball",
+    " a pillow.",
+    " a flower.",
+    " a picture.",
+    " my hair."
+  ];
+
+  function generateExcuse() {
+    var subjectIndex = Math.floor(Math.random() * subjects.length);
+    let str = subjects[subjectIndex];
+    var actionIndex = Math.floor(Math.random() * action.length);
+    str += action[actionIndex];
+    var victimIndex = Math.floor(Math.random() * victim.length);
+    str += victim[victimIndex];
+    var p = document.querySelector("#excuse");
+    p.innerText = str;
+  }
+  generateExcuse();
 };
